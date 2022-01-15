0e8c127124524f2b83b7775ebe0b5b9f854fb111
Jeremy210321 <jeremy.leon@epn.edu.ec>
2022-01-15 16:15:23 -0500

    Sprint 1 Proyecto Carcel

--- database/factories/RoleFactory.php
+++ database/factories/RoleFactory.php
@@ -2,10 +2,18 @@
 
 namespace Database\Factories;
 
+use App\Models\Role;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class RoleFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Role::class;
+
     /**
      * Define the model's default state.
      *
--- database/factories/WardFactory.php
+++ database/factories/WardFactory.php
@@ -2,20 +2,23 @@
 
 namespace Database\Factories;
 
+use App\Models\Ward;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class WardFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
+
+    protected $model = Ward::class;
+
+
     public function definition()
     {
         return [
+
             'name' => $this->faker->streetName,
+
             'location' => $this->faker->streetName,
+
             'description' => $this->faker->text($maxNbChars = 45),
         ];
     }
