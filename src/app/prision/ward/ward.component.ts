21b0615e1556d4f4b73dd852718589f6ceaf151a
Jeremy210321 <jeremy.leon@epn.edu.ec>
2022-02-28 21:15:37 -0500

    Sistema carcelarío con Angular

---
+++ src/app/prision/ward/ward.component.ts
@@ -0,0 +1,114 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ClientelaravelService } from 'src/app/service/clientelaravel.service';
+import { Ward } from 'src/app/service/Wards';
+
+@Component({
+  selector: 'app-ward',
+  templateUrl: './ward.component.html',
+  styleUrls: ['./ward.component.css']
+})
+export class WardComponent implements OnInit {
+
+  wards: Ward[] = [];
+  myFormWard!: FormGroup;
+  textBtn: string = "Register";
+  actualizar: boolean = false;
+
+  constructor(public servc: ClientelaravelService) {
+    this.servc.getWards().subscribe(r => {
+      console.warn(r);
+      console.table(r[0]);
+    })
+  }
+  ngOnInit(): void {
+    this.getWardsComponent();
+    this.myFormWard = new FormGroup({
+      idF: new FormControl(''),
+      nameF: new FormControl(''),
+      locationF: new FormControl(''),
+      descriptionF: new FormControl('')
+    });
+
+  }
+  getWardsComponent() {
+    this.servc.getWards().subscribe((r) => {
+      return this.wards = r;
+    });
+  }
+
+  addWardComponent() {
+    if (this.actualizar == false) {
+      const name = this.myFormWard.value.nameF;
+      const location = this.myFormWard.value.locationF;
+      const description = this.myFormWard.value.descriptionF;
+      this.servc.addWard(name, location, description).subscribe((r) => {
+
+        this.getWardsComponent();
+
+        this.myFormWard = new FormGroup({
+          idF: new FormControl(''),
+          nameF: new FormControl(''),
+          locationF: new FormControl(''),
+          descriptionF: new FormControl(''),
+        });
+      });
+    }
+    else {
+      const id = this.myFormWard.value.idF;
+      const name = this.myFormWard.value.nameF;
+      const location = this.myFormWard.value.locationF;
+      const description = this.myFormWard.value.descriptionF;
+      this.servc
+        .updateWard(name, location, description, id)
+        .subscribe((r) => {
+          this.getWardsComponent();
+
+          this.myFormWard = new FormGroup({
+            idF: new FormControl(''),
+            nameF: new FormControl(''),
+            locationF: new FormControl(''),
+            descriptionF: new FormControl(''),
+          });
+          this.textBtn = "Register"
+        });
+
+    }
+
+  }
+
+
+
+  updateWardComponent(id: any) {
+    this.servc.getWardById(id).subscribe((r) => {
+      this.myFormWard = new FormGroup({
+        idF: new FormControl(`${r.id}`),
+        nameF: new FormControl(`${r.name}`),
+        locationF: new FormControl(`${r.location}`),
+        descriptionF: new FormControl(`${r.description}`)
+      });
+      this.actualizar = true;
+      this.textBtn = "Update"
+    });
+  }
+
+
+  deleteWard(id: any) {
+    console.warn(id)
+
+    if (!confirm('ADVERTENCIA!!!  ESTÁ POR CAMBIAR EL ESTADO DE UN PABELLÓN')) {
+      return false;
+    }
+    else {
+      this.servc.deleteWard(id).subscribe((r) => {
+        this.getWardsComponent();
+      });
+      return true;
+    }
+  }
+
+
+
+
+
+}
---
+++ src/app/service/Wards.ts
@@ -0,0 +1,6 @@
+export interface Ward{
+    id? :number,
+    name: string, 
+    location: string, 
+    description: string
+}
