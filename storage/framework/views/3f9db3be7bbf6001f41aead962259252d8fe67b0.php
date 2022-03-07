5a6354668b5000c6d2dc18ba2aa510229cb5fac7
Jeremy210321 <jeremy.leon@epn.edu.ec>
2022-03-07 01:01:36 -0500

    Cambios en estilos

---
+++ storage/framework/views/3f9db3be7bbf6001f41aead962259252d8fe67b0.php
@@ -0,0 +1,38 @@
+<div class="container">
+    <div class="carousel">
+        <div class="carousel__face">
+            <div class="col" style="padding: 20px 40px;">
+                <div class="row"> <span>SISTEMA PENITENCIARIO </span></div>
+                <div class="row"> <span>2022</span></div>
+            </div>
+
+
+
+        </div>
+        <div class="carousel__face">
+
+        </div>
+        <div class="carousel__face">
+
+        </div>
+        <div class="carousel__face">
+
+        </div>
+        <div class="carousel__face">
+
+        </div>
+        <div class="carousel__face">
+
+        </div>
+        <div class="carousel__face">
+
+        </div>
+        <div class="carousel__face">
+
+        </div>
+        <div class="carousel__face">
+
+        </div>
+    </div>
+</div>
+<?php /**PATH C:\Users\jerem\Desktop\UNIVERSIDAD\CUARTO SEMESTRE\DESARROLLO_DE_APLICACIONES_WEB\PROGRAMAS\Proyecto-final-DAW-JL\resources\views/components/carrousel.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/8b2e8e8bf971c946ec258ffe884fb42cb810e7d9.php
@@ -0,0 +1,15 @@
+<div class="image">
+
+
+</div>
+
+<div class="text">
+    <h1 class="text-3xl font-extrabold">Prison System</h1>
+
+    
+    <p class="text-base">Web system for the management of a penitentiary center.</p>
+
+    <div class="button-contect">
+        <button class="btn-solicitar-Servicio read-more-button" onclick="myFunction()">Conócenos</button>
+    </div>
+</div><?php /**PATH C:\Users\jerem\Desktop\UNIVERSIDAD\CUARTO SEMESTRE\DESARROLLO_DE_APLICACIONES_WEB\PROGRAMAS\Proyecto-final-DAW-JL\resources\views/components/info.blade.php ENDPATH**/ ?>
